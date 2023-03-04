aff53672aafa4273a7e099ccc4c44faecf0882be
BekirK-C <b.b.kamac@gmail.com>
2023-03-04 18:11:07 +0300

    Login/out controller ve View işlemleri

---
+++ BaseProject/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(email);
+
+            if (hasUser == null)
+                return View();
+
+            var signingResult = await _signInManager.PasswordSignInAsync(hasUser, password, true, false);
+            if (!signingResult.Succeeded)
+            {
+                return View();
+            }
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
