239972698cc0daa3da0582f9d87e025e5146b91c
BekirK-C <b.b.kamac@gmail.com>
2023-03-06 13:49:56 +0300

    Strategy Pattern projesinin oluşturulması

---
+++ WebApp.Strategy/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<User>
+    {
+        //Üyelik ile ilgili tablolar IdentityDbContext'ten geliyor.
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
