3bdfc865fc9bb6b4d1c33b4b93cdfb285a92d035
BekirK-C <b.b.kamac@gmail.com>
2023-03-06 15:42:25 +0300

    1. Products migration işlemi 2. MongoDb bağlantısı ekleme

---
+++ WebApp.Strategy/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Models
+{
+    public class Product
+    {
+        [BsonId] // MongoDB için
+        [BsonRepresentation(BsonType.ObjectId)]
+        [Key] // EF Core için
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string UserId { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+    }
+}
+
+// Product sınıfında, [BsonRepresentation(BsonType.ObjectId)] özniteliği, Id özelliğinin ObjectId tipi için nasıl temsil edileceğini belirtir.
+// Bu, MongoDB veritabanındaki _id alanının ObjectId tipi ile temsil edilmesini sağlar.
