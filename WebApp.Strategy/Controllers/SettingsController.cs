884a35187e64ce46bd8f20edcdd8b27bd1565cd1
BekirK-C <b.b.kamac@gmail.com>
2023-03-10 11:12:03 +0300

    Database yolu için Settings Controller Sınıfı

---
+++ WebApp.Strategy/Controllers/SettingsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Controllers
+{
+    public class SettingsController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            Settings settings = new();
+            if (User.Claims.Where(x => x.Type == Settings.claimDatabaseType).FirstOrDefault() != null)
+            {
+                settings.databaseType = (EDatabaseType)int.Parse(User.Claims.First(x => x.Type == Settings.claimDatabaseType).Value);
+            }
+            else
+            {
+                settings.databaseType = settings.getDefaultDatabaseType;
+            }
+            return View(settings);
+        }
+    }
+}
---
+++ WebApp.Strategy/Models/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Strategy.Models
+{
+    public class Settings
+    {
+        public static string claimDatabaseType = "databasetype";
+
+        public EDatabaseType databaseType;
+        public EDatabaseType getDefaultDatabaseType => EDatabaseType.SqlServer;
+    }
+}
